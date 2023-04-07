b093764afc973429d0f73cc48c0b95b7cfd99ec4
Levi Gruspe <mail.levig@gmail.com>
2023-04-07 15:18:20 +0800

    Update packaging script

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,14 +2,15 @@
 
 # Bundle CSV, README and LICENSE files.
 
+version="0.1.1"
 ra="$(pwd)"
 temp="$(mktemp -d)"
-path="$temp/simphones-0.1.0"
+path="$temp/simphones-$version"
 mkdir -p "$path"
 cp README.md simphones.csv "$path"
 cp LICENSES/CC_BY-SA_3.0.txt "$path/LICENSE.txt"
 cd "$temp" || exit 1
-tar -czvf simphones-0.1.0.tar.gz simphones-0.1.0
-mv simphones-0.1.0.tar.gz "$ra"
+tar -czvf "simphones-$version.tar.gz" "simphones-$version"
+mv "simphones-$version.tar.gz" "$ra"
 cd "$ra" || exit 1
 rm -rf "$temp"
