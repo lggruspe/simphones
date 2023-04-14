f05a74143952dfbb40c931285b4b53b517671520
Levi Gruspe <mail.levig@gmail.com>
2023-04-14 19:30:34 +0800

    Test diacritics in inventories

--- test/test_inventories.py
+++ test/test_inventories.py
@@ -29,3 +29,34 @@ def test_parse_allophones_with_pipe() -> None:
     """Segments with a pipe should be separated into multiple segments."""
     allophones = parse_allophones("a|b c|d e")
     assert allophones == set("abcde")
+
+
+def test_diacritics() -> None:
+    """Perform tests on diacritics that appear in the PHOIBLE inventory."""
+    symbols: set[str] = set()
+    inventory = set(get_phonological_inventories()["*"].keys())
+    for sound in inventory:
+        symbols.update(sound)
+
+    for symbol in symbols:
+        assert len(symbol) == 1
+
+    # Voicelessness: only use ring below.
+    # PHOIBLE actually uses both, but `get_phonological_inventories` should be
+    # able to convert å into ḁ.
+    combining_ring_above = "\u030a"
+    combining_ring_below = "\u0325"
+    assert combining_ring_above not in symbols
+    assert combining_ring_below in symbols
+
+    # Syllabicity: only use vertical line below.
+    combining_vertical_line_below = "\u0329"
+    combining_vertical_line_above = "\u030d"
+    assert combining_vertical_line_above not in symbols
+    assert combining_vertical_line_below in symbols
+
+    # There should be no tie bars.
+    combining_double_inverted_breve = "\u0361"
+    combining_double_breve_below = "\u035c"
+    assert combining_double_inverted_breve not in symbols
+    assert combining_double_breve_below not in symbols
